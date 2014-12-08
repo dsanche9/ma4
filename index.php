de2bfe17ae2d1310e4476961a6927a8b03e2cec3
dsanche9 <dsanche9@hawk.iit.edu>
2014-12-08 17:35:51 -0600

    jah

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <!-- Name of input element determines name in $_FILES array -->
 Send this file: <input name="userfile" type="file" /><br />
 Enter Email of user: <input type="email" name="useremail"><br />
-Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
+Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone"><br />
 <input type="submit" value="Send File" />
 </form>
 <hr />
