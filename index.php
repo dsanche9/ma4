8711eb885bc6d764b606ab1e8fcb8e58ffacfe3b
dsanche9 <dsanche9@hawk.iit.edu>
2014-12-08 18:38:48 -0600

    nbv

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head><title>Hello app</title>
 </head>
 <body>
-Enter Email: <input type="email" name="useremail"><br />
+Enter Email: <input type="email" name="email"><br />
 Enter Cell Phone (1-XXX-XXX-XXXX): <input type="phone" name="phone"><br />
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="result.php" method="POST">
 <!-- MAX_FILE_SIZE must precede the file input field -->
 <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
 <!-- Name of input element determines name in $_FILES array -->
-Send this file: <input name="userfile" type="file" /><br />
+Send this file: <input name="userf" type="file" /><br />
 <input type="submit" value="Send File" />
 </form>
 <hr />
