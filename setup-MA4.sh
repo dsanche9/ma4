d00c752339633a3d4170dcb5603fea7a6326ec69
dsanche9 <dsanche9@hawk.iit.edu>
2014-12-09 10:26:07 -0600

    lo odio

--- setup-MA4.sh
+++ setup-MA4.sh
@@ -17,6 +17,7 @@ service apache2 restart 1>/tmp/01.out 2>/tmp/01.err
 mv composer.phar /var/www/html
 mv composer.json /var/www/html
 cp -r vendor /var/www/html
+rm -r vendor
 mv composer.lock /var/www/html
 mv index.php /var/www/html
 mv result.php /var/www/html
