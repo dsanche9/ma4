a551ad8bb99558fab3801286ab1e1a2be02696ac
dsanche9 <dsanche9@hawk.iit.edu>
2014-12-10 18:18:22 -0600

    jg

--- setup-MA4.sh
+++ setup-MA4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo bash
-sudo chmod 777 /var/www/html/
+
 sudo apt-get -y update 1>/tmp/01.out 2>/tmp/01.err
 sudo apt-get -y upgrade 1>/tmp/01.out 2>/tmp/01.err
 sudo apt-get -y install apache2 wget php5 php5-curl curl php5-mysql git 1>/tmp/02.out 2>/tmp/02.err
@@ -13,6 +13,7 @@ sudo cat 'extensions=json.so' >> /etc/php5/apache2/php.ini
 mv ma4/index.php index.php
 mv ma4/result.php result.php
 service apache2 restart 1>/tmp/01.out 2>/tmp/01.err
+sudo chmod 777 /var/www/html/
 
 mv composer.phar /var/www/html
 mv composer.json /var/www/html
